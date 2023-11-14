6f116146e9caf5707310468e1ee44ecb2c2d55fd
Laiz <116606017+LaizCunha@users.noreply.github.com>
2023-11-14 23:07:03 +0000

    Ajuste do componenete Tag no componenete Card

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ function App() {
   const item1 = {
     name: "Rick Sanchez",
     imageURL: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
   const item2 = {
     name: "Morty Smith",
     imageURL: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
   const item3 = {
     name: "Summer Smith",
     imageURL: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
   const items = [ item1, item2, item3]
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,15 @@ import Tag from "../Tag/Tag"
 
 export default function Card(props) {
     const item = props.item
+    const tags = item.tags || []
 
     return <div>
                 <div className="card">
                     <h2>{item.name}</h2>
                     <div className="div-tag">
-                    <Tag text="Status: Vivo"/>
-                    <Tag text="Espécie: Humana"/>
-                    <Tag text="Origem: Terra C-137"/>
+                        {tags.map(function (element) {
+                            return <Tag text={element} />
+                        })}
                 </div>
                     <img src={item.imageURL} alt="teste"/>
                 </div>

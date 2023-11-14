499527d6ef9eb52c58ce417aa573c20813d74f7d
Laiz <116606017+LaizCunha@users.noreply.github.com>
2023-11-14 00:02:26 +0000

    Criação do componente Card e alteração (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="div-tag">
-          <tag className="tag">Status:Vivo</tag>
-          <tag className="tag">Espécie:Humana</tag>
-          <tag className="tag">Origem:Terra C-137</tag>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="imagem estilo desenho de um cientista de cabelos brancos" />
-      </div>
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+    return <div>
+                <div className="card">
+                    <h2>Rick Sanchez</h2>
+                    <div className="div-tag">
+                    <tag className="tag">Status:Vivo</tag>
+                    <tag className="tag">Espécie:Humana</tag>
+                    <tag className="tag">Origem:Terra C-137</tag>
+                </div>
+                    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="imagem estilo desenho de um cientista de cabelos brancos" />
+                </div>
+            </div>
+}

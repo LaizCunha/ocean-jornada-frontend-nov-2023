e4c59b579139ccc0c8b5d9c23e27bf5d62199828
Laiz <116606017+LaizCunha@users.noreply.github.com>
2023-11-15 01:29:14 +0000

    Implementando componente ReadById

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,3 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export default function ReadById() {
-    return <div>ReadById</div>
+
+    const { id } = useParams()
+    const apiUrl = `https://rickandmortyapi.com/api/character/`
+    // IMPLEMENTAR O CARREGAMENTO DA API NA URL:
+    // GUARDAR A INFORMAÇÃO RECEBIDA DA API EM UM ESTADO
+    // EXIBIR ESSAS INFORMAÇÕES NO JSX
+
+    return <div>ReadById: { id } </div>
 }

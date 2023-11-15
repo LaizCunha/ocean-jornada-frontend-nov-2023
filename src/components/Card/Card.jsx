aeaa26bac43b6ce34310e03380cc03f43871609e
Laiz <116606017+LaizCunha@users.noreply.github.com>
2023-11-15 01:19:54 +0000

    Link adicionado ao Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom"
 import Tag from "../Tag/Tag"
 
 export default function Card(props) {
     const item = props.item
     const tags = item.tags || []
 
-    return <div className="cards">
-                <div className="card">
-                    <h2>{item.name}</h2>
-                    <div className="div-tag">
-                        {tags.map(function (element) {
-                            return <Tag text={element} />
-                        })}
+    return <Link to={`/${item.id}`}> 
+                <div className="cards">
+                    <div className="card">
+                        <h2>{item.name}</h2>
+                        <div className="div-tag">
+                            {tags.map(function (element) {
+                                return <Tag text={element} />
+                            })}
+                        </div>
+                        <img src={item.image} alt="teste"/>
+                    </div>
                 </div>
-                    <img src={item.image} alt="teste"/>
-                </div>
-            </div>
+            </Link>
 }

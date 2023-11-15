a759ae21485f9d7385274ada85c470107c9df611
Laiz <116606017+LaizCunha@users.noreply.github.com>
2023-11-15 00:40:05 +0000

    Criação do componente Home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import Card from '../Card/Card'
+import { useState } from 'react'
+
+export default function Home() {
+
+  const [items, setItems] = useState([])
+
+  async function carregarDadosApi() {
+
+    const apiUrl = "https://rickandmortyapi.com/api/character/"
+
+    const response = await fetch(apiUrl)
+
+    const body = await response.json()
+
+    const results = body.results.map(function (element) {
+      return {
+        name: element.name,
+        image: element.image,
+        tags: [
+          `Status: ${element.status}`,
+          `Species: ${element.species}`,
+          `Origin: ${element.origin.name}`,
+          `Episodes: ${element.episode.length}`
+        ]
+      }
+    })
+
+    setItems(results)
+  }
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
+  
+
+  return (
+    <>
+      {items.map(function (element) {
+        return <Card item={element} />
+      })}
+    </>
+  )
+}
